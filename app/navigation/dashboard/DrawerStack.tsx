e3d621631c7376ef20487a846defe86c5d728935
Artem Kumskov <lireddit@Artems-MacBook-Air.local>
2021-02-11 10:09:47 +0200

    add drawerstack paramlist

--- app/navigation/dashboard/DrawerStack.tsx
+++ app/navigation/dashboard/DrawerStack.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import ButtonStack from './ButtomStack';
 import CustomDrawerContent from '../../components/DrawerMenu';
-// import {DrawerStackParamList} from '../types';
+import {DrawerStackParamList} from '../types';
 
-const Drawer = createDrawerNavigator();
+const Drawer = createDrawerNavigator<DrawerStackParamList>();
 
 export default function Menu() {
   return (
