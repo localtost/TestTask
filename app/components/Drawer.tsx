8c7bd27ad715b53cd12eb5ea1573197c8bdbbedb
Artem Kumskov <lireddit@Artems-MacBook-Air.local>
2021-02-11 11:49:19 +0200

    add type in drawericon

--- app/components/Drawer.tsx
+++ app/components/Drawer.tsx
@@ -1,15 +1,10 @@
-import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 import Ionicon from 'react-native-vector-icons/Ionicons';
+import {DrawerNavigationProp} from '@react-navigation/drawer';
+import {DrawerStackParamList} from '../navigation/types';
 
-const Drawer = (): JSX.Element => {
-  const navigation = useNavigation();
-  return (
-    <Ionicon
-      size={33}
-      name="ios-menu"
-      onPress={() => navigation.openDrawer()}
-    />
-  );
+type Params = DrawerNavigationProp<DrawerStackParamList>;
+const Drawer = ({openDrawer}: Params): JSX.Element => {
+  return <Ionicon size={33} name="ios-menu" onPress={() => openDrawer()} />;
 };
 export default Drawer;
