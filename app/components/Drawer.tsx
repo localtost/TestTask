19af6dca2fd022a882cde68cd18bf21f9606fb0c
Artem Kumskov <lireddit@Artems-MacBook-Air.local>
2021-02-11 11:51:09 +0200

    return to initial

--- app/components/Drawer.tsx
+++ app/components/Drawer.tsx
@@ -1,10 +1,15 @@
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 import Ionicon from 'react-native-vector-icons/Ionicons';
-import {DrawerNavigationProp} from '@react-navigation/drawer';
-import {DrawerStackParamList} from '../navigation/types';
 
-type Params = DrawerNavigationProp<DrawerStackParamList>;
-const Drawer = ({openDrawer}: Params): JSX.Element => {
-  return <Ionicon size={33} name="ios-menu" onPress={() => openDrawer()} />;
+const Drawer = (): JSX.Element => {
+  const navigation = useNavigation();
+  return (
+    <Ionicon
+      size={33}
+      name="ios-menu"
+      onPress={() => navigation.openDrawer()}
+    />
+  );
 };
 export default Drawer;
