db318dd6ce188bb418fb4f2bc750c515fee724b1
Artem Kumskov <lireddit@Artems-MacBook-Air.local>
2021-02-14 14:23:24 +0200

    test

--- app/App.tsx
+++ app/App.tsx
@@ -5,7 +5,7 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 const App: React.FC = () => {
   return (
     <SafeAreaProvider>
-      <MainStack />
+       <MainStack />
     </SafeAreaProvider>
   );
 };
